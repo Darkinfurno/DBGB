2dd5f730ae50e68c2f188e47c1d7fc556ea8fa07
Alex Wu <alex606@att.net>
2016-06-14 21:41:32 -0500

    Replaced Tuple in Card class with CardEffect class. Moved enumerations into own class.

--- Deck_Biulding_Card_Game_Biulder/Deck_Biulding_Card_Game_Biulder/Card.cs
+++ Deck_Biulding_Card_Game_Biulder/Deck_Biulding_Card_Game_Biulder/Card.cs
@@ -7,54 +7,63 @@ using System.Threading.Tasks;
 namespace Deck_Biulding_Card_Game_Biulder
 {
     /// <summary>
-    /// 
+    /// Class to represent a CardObject
     /// </summary>
     public class Card
     {
+        // Represents the name of the Card
         public string Name
         {
             get;
             private set;
         }
+
+        // Represents how many points the card is worth at the end of the game to determine winner
         public int Points
         {
             get;
             private set;
         }
+
+        // Identification number for Card Object
         public int ID
         {
             get;
             private set;
         }
+
+        // Integer to represent the current owner of the card
         public int Owner
         {
             get;
             private set;
         }
 
-        public List<Tuple<int, int>> Effects
+        // List that represents the card effects that the card object contains
+        public List<CardEffect> Effects
         {
             get;
             private set;
         }
 
-        //something
-        public Card(string aName, int aID, int aPoints)
+        // Card Constructor
+        public Card(string aName, int aPoints, int aID, int aOwner, List<CardEffect> aCardEffects)
         {
             this.Name = aName;
-            this.ID = aID;
-            this.Effects = new List<Tuple<int, int>>();
             this.Points = aPoints;
+            this.ID = aID;
+            this.Owner = aOwner;
+            this.Effects = new List<CardEffect>();
         }
         
-        public void AddEffect(int aEffect, int aTimes)
+        public void addEffect(events aEffect, int aTimes)
         {
-            Effects.Add(new Tuple<int,int>(aEffect, aTimes));
+            Effects.Add(new CardEffect(aEffect, aTimes));
         }
 
-        public void removeEffect(int aEffect)
+        public void removeEffect(events aEffect)
         {
-            Effects.RemoveAll(x => x.Item1 == aEffect);
+            Effects.RemoveAll(x => x.Effect == aEffect);
         }
 
         public void updateOwner(int aOwner)
